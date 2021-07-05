7e9eb2d3c89b640b5ae6f254811ea15ff45da0a9
Dhina17 <dhinalogu@gmail.com>
2021-07-05 20:35:10 +0530

    config: db: Console -> console

--- config/database.js
+++ config/database.js
@@ -14,6 +14,6 @@ const options = {
 mongoose.connect(mongodb_connection_str, options).then(() => {
     console.log("Connected to mongodb successfully")
 }, err => {
-    Console.log("Database connection failed", err)
+    console.log("Database connection failed", err)
 })
 
