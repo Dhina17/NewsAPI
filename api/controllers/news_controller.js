4de2743bfd5893e6bda56f3a97c726b76fa522d6
Dhina17 <dhinalogu@gmail.com>
2021-07-06 19:41:53 +0530

    controller: Add pagination with page query

--- api/controllers/news_controller.js
+++ api/controllers/news_controller.js
@@ -6,14 +6,34 @@ const news = require("../models/news")
 exports.getAllNews = (request, response) => {
 
     // Get the query from the request.
-    // link/news?categoy={this value}
-    var query = request.query.category
+    // link/news?categoy={value}&page={value}
+
+    // category value from the query
+    var category = request.query.category
+
+    // page value from the query
+    // If the page query is empty, value will be NaN.
+    var page = parseInt(request.query.page)
+
+    // page size which I want it as static.
+    var pageSize = 10
+
+    // If page size is less than or equal to 0,
+    // Send error with code 400 bad request.
+    if (page < 0 || page === 0) {
+        err = response = {
+            "error": true,
+            "message": "invalid page number," +
+                "should start with 1"
+        };
+        response.status(400).send(err)
+    }
 
     // Declare model to hold the correct news model 
     var model
 
     // Assign the model based on category query.
-    switch (query) {
+    switch (category) {
         case 'business': {
             model = news.business
             break
@@ -44,8 +64,24 @@ exports.getAllNews = (request, response) => {
         }
     }
 
-    // Get the all news from the model and send it along with response.
-    model.find({}, (error, allNews) => {
+    // db query options to provide datas based on page.
+    var dbQuery = {}
+    // Skip the data based on page.
+    // If the page query is empty, skip = NaN (because of parseInt()).
+    // So,first {pageSize} data will be provided.
+    // If page = 1, we want to provide first 10 data.
+    // skip = 10 * (1 - 1) = 0.
+    // Here we skipped 0 data.
+    // If page = 2, we want to provide second 10 data.
+    // skip = 10 * (2 - 1) = 10.
+    // Here we skip first 10 data (i.e) first page.
+    dbQuery.skip = pageSize * (page - 1)
+    // we are limiting the size of the data (i.e) page size.
+    // Here want only 10 datas per page.
+    dbQuery.limit = pageSize
+
+    // Get news from the model based on dbQuery options and send it along with response.
+    model.find({}, {}, dbQuery, (error, allNews) => {
 
         // If error in find(), send the error response with code 500 (Internal Server Error).
         if (error) {
